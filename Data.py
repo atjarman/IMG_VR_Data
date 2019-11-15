be7a5655e41ec581b4e28c7614706937bb3c94d3
Andrew Jarman <atjuegos@gmail.com>
2019-11-15 06:57:36 -0700

    Created dictionary with samples to more easily insert new headers

--- Data.py
+++ Data.py
@@ -1,6 +1,105 @@
 import gzip
 import os
 import csv
+import json
+
+dir = "C:/Users/Owner/Desktop/School/BMI 483/IMG_VR_download/IMG_VR"
+
+old = open('C:/Users/Owner/Desktop/School/BMI 483/IMG_VR_download/IMG_VR/Test/IMGVR_Sequence_information.tsv.txt', 'r')
+new = open('C:/Users/Owner/Desktop/School/BMI 483/IMG_VR_download/IMG_VR/Test/IMGVR_all_Sequence_information.tsv.txt', 'r')
+outfile = open('C:/Users/Owner/Desktop/School/BMI 483/IMG_VR_download/IMG_VR/Test/new.txt', 'w')
+
+counter1 = 0
+masterList = []
+headerList1 = []
+
+for line in old:
+    temp = {}
+    if counter1 == 0:
+        line = line.strip('\n')
+        headerList1 = line.split('\t')
+        counter1 += 1
+    else:
+        line = line.strip('\n')
+        lineList1 = line.split('\t')
+        keyCount = 0
+        for x in lineList1:
+            temp[headerList1[keyCount]] = x
+            keyCount += 1
+        counter1 += 1
+        masterList.append(temp)
+
+counter2 = 0
+tempList = []
+headerList2 = []
+
+for line in new:
+    if counter2 == 0:
+        line = line.strip('\n')
+        headerList2 = line.split('\t')
+        counter2 += 1
+    else:
+        line = line.strip('\n')
+        lineList2 = line.split('\t')
+        counter2 += 1
+        tempList.append(lineList2)
+
+difference = set(headerList2) - set(headerList1)
+same = set(headerList2) & set(headerList1)
+
+headerDifPositionDict = {}
+for x in difference:
+       headerDifPositionDict[x] = headerList2.index(x)
+
+headerSamePositionDict = {}
+for x in same:
+    headerSamePositionDict[x] = headerList2.index(x)
+
+headerDifPositionList = list(headerDifPositionDict.values())
+headerSamePositionList = list(headerSamePositionDict.values())
+
+tempNewDict2 = {}
+
+for x in tempList:
+    tempNewDict = {}
+    iD = x[headerList1.index("mVCs")]
+
+    for y in masterList:
+        present = False
+        if list(y.values()).__contains__(iD):
+            present = True
+            for z in headerSamePositionList:
+                if x[z] != y[headerList2[z]]:
+                    tempNewDict[headerList2[z]] = x[z]
+            break
+
+    if not present:
+        for y in headerList2:
+            tempNewDict[y] = x[headerList2.index(y)]
+        tempNewDict2[x[headerList1.index("mVCs")]] = tempNewDict
+    else:
+        for y in headerDifPositionList:
+            tempNewDict[headerList2[y]] = x[y]
+        tempNewDict2[x[headerList1.index("mVCs")]] = tempNewDict
+
+#print(json.dumps(tempNewDict2, indent=4, sort_keys=True))
+
+
+#for x in tempNewDict2:
+#    print(x)
+
+#test = json.dumps(tempNewDict2, indent=4, sort_keys=True)
+
+
+
+old.close()
+new.close()
+outfile.close()
+
+
+
+
+
 
 """
 dir = "C:/Users/Owner/Desktop/School/BMI 483/IMG_VR_download/IMG_VR"
@@ -52,13 +151,89 @@ for header in newHeaders:
 print(mainList)
 """
 
+"""
+print(tempNewDict2)
+print(headerList1)
+print(masterList)
+print("----------------------------------")
+print(headerList2)
+print(tempList)
+"""
 
+"""
+for x in headerList2:
+    if not headerList1.__contains__(x):
+        print(x, headerList2.index(x))
+"""
+
+"""
+for x in tempList:
+    iD = x[headerList1.index("mVCs")]
+
+    for y in masterList:
+        present = False
+        if list(y.values()).__contains__(iD):
+            present = True
+            break
+    if not present:
+        print(x)
+        print("true")
+"""
+
+"""
+print(headerSamePositionList)
+for x in tempList:
+    iD = x[headerList1.index("mVCs")]
+    for y in masterList:
+        if list(y.values()).__contains__(iD):
+            for z in headerSamePositionList:
+                if x[z] != y[headerList2[z]]:
+                    print(False, x[z], y[headerList2[z]])
+
+    #print(x)
+"""
+
+"""
+newSampleList = []
+for x in tempList:
+    present = False
+    for y in masterList:
+        if y.__contains__(x[0]):
+            present = True
+            print(x)
+            print(y)
+            print(set(x) & set(y))
+            if x == y:
+                print("True")
+            else:
+                print("False")
+    if not present:
+        newSampleList.append(x)
+"""
+
+"""
+import os
+# Read in the original and new file
+orig = open('C:/Users/Owner/Desktop/School/BMI 483/IMG_VR_download/IMG_VR/Test/IMGVR_Sequence_information.tsv.txt', 'r')
+new = open('C:/Users/Owner/Desktop/School/BMI 483/IMG_VR_download/IMG_VR/Test/IMGVR_all_Sequence_information.tsv.txt', 'r')
+#in new but not in orig
+bigb = set(new) - set(orig)
+# To see results in console if desired
+print(bigb)
+# Write to output file
+with open('different.csv', 'w') as file_out:
+    for line in bigb:
+        file_out.write(line)
+#close the files
+orig.close()
+new.close()
+file_out.close()
+"""
+
+"""
 infile1 = open('C:/Users/Owner/Desktop/School/BMI 483/IMG_VR_download/IMG_VR/Test/IMGVR_Sequence_information.tsv.txt', 'r')
 infile2 = open('C:/Users/Owner/Desktop/School/BMI 483/IMG_VR_download/IMG_VR/Test/IMGVR_all_Sequence_information.tsv.txt', 'r')
 outfile = open('C:/Users/Owner/Desktop/School/BMI 483/IMG_VR_download/IMG_VR/Test/new.txt', 'w')
-
-import re
-
 
 tree = {}
 
@@ -78,10 +253,14 @@ for line in infile1:
         for x in lineList:
             sample[headerList[counter2]] = lineList[counter2]
             counter2 += 1
+
     print(sample)
 
-
-
+print(tree)
+infile1.close()
+infile2.close()
+outfile.close()
+"""
 
 """
 linecounter = 0
@@ -96,15 +275,6 @@ for line in infile2:
             outfile.write(line + '\n')
     linecounter += 1
 """
-
-infile1.close()
-infile2.close()
-outfile.close()
-
-
-
-
-
 
 """
 for x in os.listdir(dir):
@@ -169,6 +339,3 @@ for x in os.listdir(dir):
                                print(line)
                                counter2 += 1
 """
-
-#print(y)
-"TESTING"
